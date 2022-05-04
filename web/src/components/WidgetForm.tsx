375e1e86893b4fda00c6bcbcda65d0e30369fc2c
vitorjobs <vittorjobs@gmail.com>
2022-05-03 21:50:21 -0300

    style: button close style

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,3 +1,5 @@
+import { CloseButton } from "./CloseButton";
+
 export function WidgetForm(){
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto ">
@@ -6,6 +8,7 @@ export function WidgetForm(){
           Deixe seu feedback
         </span>
       
+      <CloseButton/>
       </header>
 
         <p> Ola</p>
