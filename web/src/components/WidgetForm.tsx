3dbf03666fe4962259a6456026ba19d43573ffe1
vitorjobs <vittorjobs@gmail.com>
2022-05-04 15:09:58 -0300

    style: style of component button

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,4 +1,31 @@
 import { CloseButton } from "./CloseButton";
+import bugImageUrl from '../assets/bug.svg'
+import ideaImageUrl from '../assets/idea.svg';
+import thoughtImageUrl from '../assets/thought.svg';
+
+const feedbackTypes = {
+  BUG: {
+    title: 'Problem',
+    image: {
+        source: bugImageUrl,
+        alt: 'Imagem de um inseto'
+    }
+  },
+  IDEA: {
+    title: 'Idia',
+    image: {
+        source: ideaImageUrl,
+        alt: 'Imagem de uma lâmpada'
+    }
+  },
+  OTHER: {
+    title: 'Other',
+    image: {
+        source: thoughtImageUrl,
+        alt: 'Imagem de um balão de pensamento'
+    }
+  },
+  }
 
 export function WidgetForm(){
   return (
@@ -11,7 +38,25 @@ export function WidgetForm(){
       <CloseButton/>
       </header>
 
-        <p> Ola</p>
+       <div className="flex py-8 gap-2 w-full">
+        {
+          Object.entries(feedbackTypes).map(([key, value]) => {
+
+            return (
+              <button 
+              key={key}
+              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+              // onClick={}
+              type="button"
+              >
+                <img src={value.image.source} alt={value.image.alt}></img> 
+                <span>{value.title}</span>
+              </button>
+            ) 
+          })
+        }
+
+       </div>
 
       <footer className="text-xs text-neutral-400">
         Feito com carinho por <a 
