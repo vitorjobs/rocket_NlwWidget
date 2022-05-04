6354de0127695d8558aed81eecd7502a73257c25
vitorjobs <vittorjobs@gmail.com>
2022-05-03 21:36:49 -0300

    style: footer of widget

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,7 +1,22 @@
 export function WidgetForm(){
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto ">
-      Olá Mundo
+      <header>
+        <span className="text-xl leading-6">
+          Deixe seu feedback
+        </span>
+      
+      </header>
+
+        <p> Ola</p>
+
+      <footer className="text-xs text-neutral-400">
+        Feito com carinho por <a 
+            className="underline underline-offset-2"
+            href="https://github.com/vitorjobs">
+            Vítor Guedes
+          </a>
+      </footer>
     </div>
   )
 }
