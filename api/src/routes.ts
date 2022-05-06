8b8cf434d8ed366e0e48345b31b0816d1897cf54
vitorjobs <vittorjobs@gmail.com>
2022-05-06 20:33:52 -0300

    feat: create routes file

---
+++ api/src/routes.ts
@@ -0,0 +1,39 @@
+import express from 'express'
+import nodemailer from 'nodemailer'
+import { prisma } from './prisma'
+
+export const routes = express.Router()
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "6309166e4e1790",
+    pass: "efdd99a8e5eea8"
+  }
+});
+
+routes.post('/feedbacks', async (req, res) =>{
+
+  const {type, comment, screenshot} = req.body
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot
+    }
+  })
+
+  await transport.sendMail({
+    from: 'Equipe Widget <oi@teste.com>',
+    to: 'Vitor Widget <vittorbassdev@gmail.com',
+    subject: 'Novo Feedback',
+    html: [
+      `<p> Tipo de Feedback: ${type}</p>`,
+      `<p> Comentário de Feedback ${comment}</p>`,
+    ].join('\n')
+  })
+
+  return res.status(201).json({data : feedback})
+})
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,44 +1,11 @@
 import express from 'express'
-import nodemailer from 'nodemailer'
-import { prisma } from './prisma'
+import { routes } from './routes'
 
 const app = express()
 
 app.use(express.json())
 
-const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "6309166e4e1790",
-    pass: "efdd99a8e5eea8"
-  }
-});
-
-app.post('/feedbacks', async (req, res) =>{
-
-  const {type, comment, screenshot} = req.body
-
-  const feedback = await prisma.feedback.create({
-    data: {
-      type,
-      comment,
-      screenshot
-    }
-  })
-
-  await transport.sendMail({
-    from: 'Equipe Widget <oi@teste.com>',
-    to: 'Vitor Widget <vittorbassdev@gmail.com',
-    subject: 'Novo Feedback',
-    html: [
-      `<p> Tipo de Feedback: ${type}</p>`,
-      `<p> Comentário de Feedback ${comment}</p>`,
-    ].join('\n')
-  })
-
-  return res.status(201).json({data : feedback})
-})
+app.use(routes)
 
 app.listen(3333, ()=>{
   console.log("Server is running")
